485f4ef0db950122402b5ffa635d562b9c437061
foreachsky <78392742@qq.com>
2017-07-19 14:45:14 +0800

    add the file

---
+++ setup.go
@@ -0,0 +1,11 @@
+package picauth
+import(
+    "github.com/mholt/caddy"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+)
+
+func init(){
+    caddy.RegisterPlugin("picauth",caddy.Plugin{
+        })
+}
+
